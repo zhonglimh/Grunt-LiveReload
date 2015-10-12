1c4910983aa4b97a35d1571b69f464789170dfef
Ryan <zhong_li03@hotmail.com>
2015-10-12 11:21:00 +0800

    基于 grunt-contrib-connect 0.11.x 版本
    更新Gruntfile.js

--- Example 2/Gruntfile.js
+++ Example 2/Gruntfile.js
@@ -7,14 +7,15 @@ module.exports = function(grunt) {
 		port: lrPort
 	});
 	// 使用 middleware(中间件)，就必须关闭 LiveReload 的浏览器插件
-	var lrMiddleware = function(connect, options) {
+	var serveStatic = require('serve-static');
+	var serveIndex = require('serve-index');
+	var lrMiddleware = function(connect, options, middlwares) {
 		return [
-			// 把脚本，注入到静态文件中
 			lrSnippet,
 			// 静态文件服务器的路径
-			connect.static(options.base[0]),
+			serveStatic(options.base[0]),
 			// 启用目录浏览(相当于IIS中的目录浏览)
-			connect.directory(options.base[0])
+			serveIndex(options.base[0])
 		];
 	};
 
